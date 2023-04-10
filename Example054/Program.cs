cc0c632751a5b3314ef946413ebeb0b1f909dc97
Vitaly <vitalik.yakhimovich@mail.ru>
2023-04-10 18:33:49 +0300

    add Example 054

---
+++ Example054/Program.cs
@@ -0,0 +1,56 @@
+﻿
+/* Задайте двумерный массив.
+ Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+ */ 
+void Random(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void Sort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+ int[,] table = new int[3, 4];
+
+Random(table);
+
+PrintArray(table);
+
+Sort(table);
+
+Console.WriteLine();
+PrintArray(table);
